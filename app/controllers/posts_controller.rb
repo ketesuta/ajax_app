2b116a29b2387febb9bff3f75ae1f4e940853117
森田一 <moritahajime@moritahajimenoMacBook-Air.local>
2020-11-05 17:44:04 +0900

    Ajaxappの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,11 @@
 class PostsController < ApplicationController
 
   def index #indexアクション
-    @posts = Post.all #一番目のレコードを@postに代入
-  end
-
-  def new
-    
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content])
   end
+
 end
